03665db36eef3c2d04565e69c5b2260da4c5183b
Аслан Нурсеитов <grooves97@gmail.com>
2019-06-27 12:04:08 +0600

    Передача скриншота. Переделать отправку с сервера на клиента

---
+++ UDPClient/UDPClient/MainWindow.xaml.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.IO;
+using Microsoft.Win32;
+
+namespace UDPClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private long onBlockSize;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            onBlockSize = 1500;// MTU = 1500 не больше 64 т.
+        }
+
+        private void SelectFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter= "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+
+            if (openFileDialog.ShowDialog()==true)
+            {
+                FileNameTextBox.Text = openFileDialog.FileName;
+            }
+        }
+
+        private void SendButton_Click(object sender, RoutedEventArgs e)
+        {
+            EndPoint serverEndPoint = new IPEndPoint
+                (IPAddress.Parse(ServerIpTextBox.Text), 
+                int.Parse(PortTextBox.Text));//Чтение файла
+
+            using (FileStream inputFile = new FileStream(FileNameTextBox.Text, FileMode.Open))
+            {
+                using (Socket serverSocket = new Socket(AddressFamily.InterNetwork,SocketType.Dgram, ProtocolType.Udp))
+                {
+                    long fileSize = inputFile.Length;
+                    long countRead = fileSize / onBlockSize;
+                    long RemaindSize = fileSize % onBlockSize;
+                    byte[] buf = new byte[onBlockSize];
+
+                    //уведомить получателя о количестве частей файла
+                    int countSend = (int)countRead;
+
+                    if (RemaindSize > 0)
+                        RemaindSize++;
+
+                    serverSocket.SendTo(BitConverter.GetBytes(countSend), serverEndPoint);
+
+                    for (long i = 0; i < countRead; i++)
+                    {
+                        inputFile.Read(buf, 0, (int)onBlockSize);
+                        serverSocket.SendTo(buf, 0, serverEndPoint);
+                        Thread.Sleep(10);
+                    }
+
+                    if (RemaindSize != 0)
+                    {
+                        inputFile.Read(buf, 0, (int)RemaindSize);
+                        serverSocket.SendTo(buf, 0, (int)RemaindSize, SocketFlags.None, serverEndPoint);
+                    }
+                }
+            }
+        }
+
+    }
+}
---
+++ UDPServer/UDPServer/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.Sockets;
+using System.Net;
+using System.IO;
+
+namespace UDPServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] buf = new byte[64*1024];//Максимальный размер доступный для UDP
+            long fullSize = 0;
+            int countFileBlock = 0;
+            int port = 12345;
+            byte[] countFileBlockBytes = new byte[8];//Получчение количетсва файлов для приема по UDP
+
+            Socket serverSocket = new Socket
+                (AddressFamily.InterNetwork, 
+                SocketType.Dgram, ProtocolType.Udp);
+
+            EndPoint sourceEndPoint = new IPEndPoint(0, 0);
+
+            FileStream outFile = new FileStream("outputFile.bin", FileMode.Create);
+
+            EndPoint serverEndPoint = new IPEndPoint(IPAddress.Any, port);
+            serverSocket.Bind(serverEndPoint);
+
+            int size = serverSocket.ReceiveFrom(countFileBlockBytes, ref sourceEndPoint);
+            countFileBlock = BitConverter.ToInt32(countFileBlockBytes, 0);//Чтение частей файла
+
+            Console.WriteLine($"Count file block {countFileBlock}");
+
+            int i = 0;
+            while (countFileBlock > 0)
+            {
+                size = serverSocket.ReceiveFrom(buf, ref sourceEndPoint);
+                Console.WriteLine($"Received size = {size}");
+                outFile.Write(buf, 0, size);
+                fullSize += size;
+                Console.WriteLine($"{i}Write size = {fullSize}");
+                i++;
+                countFileBlock--;
+            }
+            
+        }
+    }
+}
